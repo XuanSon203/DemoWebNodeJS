30b8a150037dc2cb569ebded39249f1f6f18e98f
Xuanson <pxuanson203@gmail.com>
2024-12-25 21:56:24 +0700

    Edit session

--- router/admin/product.js
+++ router/admin/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // read  data file from system path
 const multer = require("multer");
-// const strorageMulter = require("../../helpers/storageMulter");
+const strorageMulter = require("../../helpers/storageMulter");
 const validate = require("../../validate/admin/product_validate");
 const router = express.Router();
 const cloudinary = require("cloudinary").v2;
@@ -29,27 +29,6 @@ router.get("/edit/:id", productController.edit);
 router.post(
   "/create",
   upload.single("image"),
-  function (req, res, next) {
-    let streamUpload = (req) => {
-      return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream((error, result) => {
-          if (result) {
-            resolve(result);
-          } else {
-            reject(error);
-          }
-        });
-
-        streamifier.createReadStream(req.file.buffer).pipe(stream);
-      });
-    };
-    async function upload(req) {
-      let result = await streamUpload(req);
-      console.log(result);
-      console.log(result)
-    }
-    upload(req);
-  },
   validate.createPost,
   productController.createPost
 );
